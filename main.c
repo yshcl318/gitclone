8f0e00d27f36227d2ff5af24da047841fae57c55
yshcl318 <536558334@qq.com>
2024-10-18 23:48:52 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,61 +1,41 @@
-/*#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
-}*/
+//#include <stdio.h>
+//int main()
+//{
+//    int a,b,c,z;
+//    printf("输入三个数：");
+//    scanf("%d%d%d",&a,&b,&c);
+//    if (a>b){
+//            z=a,a=b,b=z;
+//    }
+//    if (a>c){
+//        z=a,a=c,c=z;
+//    }
+//    printf("%d\n",a);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main(){
+//    int a,b,x;
+//    double c;
+//    scanf("%d%d/n",&a,&b);
+//    c=a+0.1*b;
+//    x=c/1.9;
+//    printf("%d",x);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main(){
+//    printf("  *\n");
+//    printf(" ***\n");
+//    printf("*****\n");
+//    printf(" ***\n");
+//    printf("  *\n");
+//     return 0;
+//}
 #include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-void menu()
-{
-    printf("************************\n");
-    printf("*******  1. play   *****\n");
-    printf("*******  0. exit   *****\n");
-    printf("************************\n");
-}
-void game()
-{
-    int ret=rand()%100+1;
-    int guess=0;
-    while(1)
-    {
-        printf("请猜数字：");
-        scanf("%d",&guess);
-        if(guess>ret)
-            printf("猜大了\n");
-        else if(guess<ret)
-            printf("猜小了\n");
-        else
-        {
-            printf("猜对了\n");
-            break;
-        }
-    }
-}
-int main()
-{
-    int input=0;
-    srand((unsigned int)time(NULL));
-    do
-    {
-        menu();
-        printf("请选择:");
-        scanf("%d",&input);
-        switch(input)
-        {
-        case 1:
-            game();
-            break;
-        case 0:
-            printf("退出游戏\n");
-            break;
-        default:
-            printf("选择错误，请重新选择\n");
-            break;
-        }
-    }while(input);
+int main(){
+    char q,w,e,r,t;
+    scanf("%c%c%c%c%c",&q,&w,&e,&r,&t);
+    printf("%c%c%c%c%c",t,r,e,w,q);
     return 0;
 }
