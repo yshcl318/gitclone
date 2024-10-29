a3b47afcbeb1f9ab3aea20cee07800af57a87c1e
yshcl318 <536558334@qq.com>
2024-10-29 22:38:59 +0800

    h

---
+++ test/10/main.c
@@ -0,0 +1,19 @@
+/*#include <stdio.h>
+#include <stdlib.h>
+void add(int*p)
+{
+    *p=*p+1;
+}
+int main()
+{
+    int num=0;
+    add(&num);
+    printf("%d",num);
+    return 0;
+}*/
+#include <stdio.h>
+int main()
+{
+    printf("%d",printf("%d",printf("%d",43)));
+    return 0;
+}
