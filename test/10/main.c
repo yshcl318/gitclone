3ea048b16c34b993b1a34831e1c93a8882c96b26
yshcl318 <536558334@qq.com>
2024-10-30 22:41:27 +0800

    h

--- test/10/main.c
+++ test/10/main.c
@@ -11,9 +11,41 @@ int main()
     printf("%d",num);
     return 0;
 }*/
-#include <stdio.h>
+/*#include <stdio.h>
 int main()
 {
     printf("%d",printf("%d",printf("%d",43)));
+    return 0;
+}*/
+/*#include <stdio.h>
+void print(int n)
+{
+    if(n>9)
+        print(n/10);
+    printf("%d ",n%10);
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    print(n);
+    return 0;
+}*/
+int my_sizeof(char*str)
+{
+    int count=0;
+    while(*str!='\0')
+    {
+        count++;
+        str++;
+    }
+    return count;
+}
+#include<stdio.h>
+int main()
+{
+    char arr[]="abc";
+    size_t len=my_sizeof(arr);
+    printf("%zd\n",len);
     return 0;
 }
