56390df1ecb1f5e0b7cf0c31ef3d880d081b9ab0
yshcl318 <536558334@qq.com>
2024-11-04 21:05:10 +0800

    h

--- test/15/main.c
+++ test/15/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 
 int main()
@@ -29,4 +29,63 @@ int main()
     printf("去掉一个最高分:%.0f,去掉一个最低分:%.0f\n",score[9],score[0]);
     printf("平均分为:%.1f",sum/8);
     return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    switch(a)
+    {
+        case 1:printf("1");break;
+        case 2:printf("2");break;
+        case 3:printf("3");break;
+        case 4:printf("4");
+        default:printf("5");
+    }
+    return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{
+    int a[6][5];
+    int i=0,j=0;
+    for(i=0;i<6;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            a[i][j]=i*5+j+1;
+        }
+    }
+    for(i=0;i<6;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            printf("%d  ",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}*/
+#include<stdio.h>
+int main()
+{
+    int a[6][5];
+    int i=0,j=0;
+    for(i=0;i<6;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            a[i][j]=i*5+j+1;
+        }
+    }
+    for(i=0;i<6;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            printf("&a[%d][%d]=%p\n",i,j,&a[i][j]);
+        }
+    }
+    return 0;
 }
+
