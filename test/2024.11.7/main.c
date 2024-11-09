dc5a73dd560be60fd4a62896f3682ea665e1bf74
yshcl318 <536558334@qq.com>
2024-11-09 13:16:13 +0800

    h

--- test/2024.11.7/main.c
+++ test/2024.11.7/main.c
@@ -34,25 +34,12 @@ int main()
     }
     printf("%d",a);
 }*/
-#define row 9
-#define col 9
-#define rows row+2
-#define cols col+2
-void menu(int i)
-{
-    printf("****************\n");
-    printf("**** 1.play ****\n");
-    printf("**** 0.exit ****\n");
-    printf("****************\n");
-}
-void game()
-{
-    char mine[rows][cols];
-    char show[rows][cols];
-}
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 int main()
 {
+    srand((unsigned int)time(NULL));
     int i=0;
     do
     {
@@ -72,4 +59,112 @@ int main()
             printf("选择错误，请重新选择\n");
     }while(i);
     return 0;
+}
+void menu(int i)
+{
+    printf("****************\n");
+    printf("**** 1.play ****\n");
+    printf("**** 0.exit ****\n");
+    printf("****************\n");
+}
+void game()
+{
+    int row=9,col=9;
+    int rows=row+2,cols=col+2;
+    char mine[rows][cols];
+    char show[rows][cols];
+    initboard(rows,cols,'0',mine);
+    initboard(rows,cols,'*',show);
+    displayboard(row,col,show);
+    seyboard(row,col,mine);
+    findboard(row,col,rows,cols,mine,show);
+}
+
+void initboard(int rows,int cols,char set,char mine[rows][cols])
+{
+    int i=0,j=0;
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<cols;j++)
+        {
+            mine[i][j]=set;
+        }
+    }
+}
+
+void displayboard(int row,int col, char board[row][col])
+{
+    int i=0,j=0;
+    printf("------扫雷------\n");
+    for(i=0;i<=col;i++)
+    {
+        printf("%d",i);
+    }
+    printf("\n");
+    for(i=1;i<=row;i++)
+    {
+        printf("%d",i);
+        for(j=1;j<=col;j++)
+        {
+            printf("%c",board[i][j]);
+        }
+        printf("\n");
+    }
+     printf("------扫雷------\n");
+}
+
+#define easy 10
+void seyboard(int row,int col,char mine[row][col])
+{
+    int count=easy;
+    while(count)
+    {
+        int x=rand()%row+1;
+        int y=rand()%row+1;
+        if(mine[x][y]=='0')
+        {
+            mine[x][y]='1';
+            count--;
+        }
+    }
+}
+void findboard(int row,int col,int rows,int cols,char mine[rows][cols],char show[rows][cols])
+{
+    int x=0,y=0;
+    int win=0;
+     while (win<row*col-easy)
+     {
+        printf("请输入要排查的坐标:");
+        scanf("%d %d",&x,&y);
+            if(x>=1&&x<=row&&y>=1&&y<=col)
+            {
+                if(mine[x][y]=='1')
+                {
+                    printf("很遗憾，你输了\n");
+                    displayboard(row,col,mine);
+                    break;
+                }
+                else
+                {
+                    int c=getminecount(x,y,rows,cols,mine);
+                    show[x][y]=c+'0';
+                    displayboard(row,col,show);
+                    win++;
+                }
+            }
+            else
+            {
+                printf("坐标非法，重新输入\n");
+            }
+     }
+     if(win==row*col-easy)
+     {
+         printf("胜利\n");
+         displayboard(row,col,mine);
+     }
+}
+void getminecount(int x,int y,int rows,int cols,char mine[rows][cols])
+{
+
+    return mine[x-1][y]+mine[x-1][y-1]+mine[x][y-1]+mine[x+1][y-1]+mine[x+1][y]+mine[x+1][y+1]+mine[x][y+1]+mine[x-1][y+1]-8*'0';
 }
