9a22ad689a4fc216fbe000d982578dbbcf349dd4
yshcl318 <536558334@qq.com>
2024-11-22 16:21:45 +0800

    h

--- test/2024.11.22/main.c
+++ test/2024.11.22/main.c
@@ -575,7 +575,7 @@ int main() {
 
     return 0;
 }*/
-#include<stdio.h>
+/*#include<stdio.h>
 int main()
 {
     int a,b,j=0;
@@ -600,4 +600,39 @@ int main()
         }
     }
     return 0;
+}*/
+/*#include <stdio.h>
+#include <math.h>
+#define PI acos(-1)
+#define eps 1e-7
+double f(double);
+int main()
+{
+	double y;
+	scanf("%lf", &y);
+	double l = 0.33, r = 10, mid;
+	while (l < r) //判断区间是否可以继续二分
+	{
+		mid = (l + r) / 2; //取区间的中点
+		if (fabs(f(mid) - y) < eps) //若已经满足精度要求，则mid即为方程的解
+			break;
+		if (f(mid) < y + eps)
+			r = mid;
+		else
+			l = mid;
+	}
+	printf("%.5f\n", mid);
+	return 0;
+}
+double f(double x)
+{
+    double y=(sin(sqrt(x))+exp(-pow(x,1.0/3)))/log(PI*x);
+    return y;
+}
+*/
+#include<stdio.h>
+int main()
+{
+    char a[3];
+
 }
