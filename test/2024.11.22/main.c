e236dd1377ba4331933c46869ccc1a0fced362a5
yshcl318 <536558334@qq.com>
2024-11-22 21:28:06 +0800

    h

--- test/2024.11.22/main.c
+++ test/2024.11.22/main.c
@@ -633,6 +633,148 @@ double f(double x)
 #include<stdio.h>
 int main()
 {
-    char a[3];
-
+    int a=0,b=0,s=1;
+    char c;
+    while(scanf("%d %c",&a,&c)!=EOF)
+    {
+        s=1;
+        if(c=='!')
+        {
+            if(a==0)
+                printf("1\n");
+            else
+                {
+                    for(int i=1;i<=a;i++)
+                    {
+                        s=s*i;
+                    }
+                    printf("%d\n",s);
+                }
+        }
+        else
+        {
+            scanf(" %d",&b);
+            if(c=='+')
+            {
+                s=a+b;
+                printf("%d\n",s);
+            }
+            else if(c=='-')
+            {
+                s=a-b;
+                printf("%d\n",s);
+            }
+            else if(c=='*')
+            {
+                s=a*b;
+                printf("%d\n",s);
+            }
+            else if(c=='/')
+                {
+                    if(b!=0)
+                    {
+                        s=a/b;
+                        printf("%d\n",s);
+                    }
+                    else
+                    {
+                        printf("error\n");
+                    }
+                }
+            else if(c=='%')
+                {
+                    if(b!=0)
+                    {
+                        s=a%b;
+                        printf("%d\n",s);
+                    }
+                    else
+                    {
+                        printf("error\n");
+                    }
+                }
+            else
+            {
+                printf("error\n");
+            }
+        }
+    }
+    return 0;
 }
+/*#include <stdio.h>
+
+// 函数声明
+int calculate(char op, int a, int b);
+int factorial(int n);
+
+int main() {
+    char op;
+    int a, b;
+    int result;
+
+    // 循环读取输入直到遇到文件结束符EOF
+    while (scanf("%d %c", &a, &op) != EOF) {
+        if (op == '!') {
+            // 阶乘运算
+            result = factorial(a);
+            printf("%d\n", result);
+            continue;
+        }
+        // 读取第二个整数
+        scanf("%d", &b);
+        switch (op) {
+            case '+':
+                result = calculate(op, a, b);
+                break;
+            case '-':
+                result = calculate(op, a, b);
+                break;
+            case '*':
+                result = calculate(op, a, b);
+                break;
+            case '/':
+                if (b == 0) {
+                    printf("error\n");
+                    continue;
+                }
+                result = calculate(op, a, b);
+                break;
+            case '%':
+                if (b == 0) {
+                    printf("error\n");
+                    continue;
+                }
+                result = calculate(op, a, b);
+                break;
+            default:
+                printf("error\n");
+                continue;
+        }
+        printf("%d\n", result);
+    }
+    return 0;
+}
+
+// 计算函数
+int calculate(char op, int a, int b) {
+    switch (op) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+        case '%':
+            return a % b;
+        default:
+            return 0; // 不应该到达这里
+    }
+}
+
+// 阶乘函数
+int factorial(int n) {
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
+}*/
