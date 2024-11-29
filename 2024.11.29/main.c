c3202efadb6d0b37ad44a4fda5750ffba11d31f5
yshcl318 <536558334@qq.com>
2024-11-29 15:01:13 +0800

    h

--- 2024.11.29/main.c
+++ 2024.11.29/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 void Rua();
 int main() {
 	Rua();//µ÷ÓÃµÝ¹éº¯Êý
@@ -6,12 +6,30 @@ int main() {
 }
 
 void Rua() {
-	/**********Begin**********/
+
 	int a[100],i,j;
-	for (i=0;scanf("%d ",&a[i])==1;i++);
-	for(j=i;j>=0;j--)
+	for (i=0;scanf("%d",&a[i])==1;i++);
+	for(j=i-1;j>=0;j--)
     {
         printf("%d\n",a[j]);
     }
-	/**********End**********/
+
+}*/
+#include <stdio.h>
+int ack(int, int);
+int main() {
+	int m, n;
+	scanf("%d%d", &m, &n);
+	printf("%d", ack(m, n));
+	return 0;
+}
+
+int ack(int m, int n) {
+	if(m==0)
+        return n+1;
+    else if(m>0&&n==0)
+        return ack(m-1,1);
+    else if(m>0&&n>0)
+        return ack(m-1,ack(m,n-1));
+
 }
