292288f4b9d92312d9cab4015dca9e4b51b62bac
yshcl318 <536558334@qq.com>
2024-12-18 18:57:33 +0800

    h

--- 2024.12.17/main.c
+++ 2024.12.17/main.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/*#include<stdio.h>
 int main()
 {
     int i=0,n=0,a=0,b=0,c=0;
@@ -25,5 +25,39 @@ int main()
         }
     }
     printf("%d",c);
+    return 0;
+}*/
+//#include<stdio.h>
+//int main()
+//{
+    // 请在此添加您的代码
+/*    /********** Begin *********/
+/*        int n=0,i=0;
+        scanf("%d",&n);
+        for(i=10000;i>=n;i--)
+        {
+            if(i%n==0)
+            {
+                printf("%d",i);
+                break;
+            }
+        }
+    /********** End *********/
+//    return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+    int n,i=0,m,j=0;
+    scanf("%d",&n);
+    int num[n];
+    for(i=0;i<n;i++)
+        scanf("%d ",&num[i]);
+    scanf("%d",&m);
+    for(i=0;i<n;i++)
+        if(num[i]==m)
+            j++;
+    printf("%d",j);
     return 0;
 }
